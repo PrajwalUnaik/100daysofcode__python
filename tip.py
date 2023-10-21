2613bc3ca8f0344ac3166d89594d818aa31f7936
Prajwal U <Prajwalu28@protonmail.com>
2023-10-21 14:55:13 +0530

    Tip Calculator project 💁‍♂️
    🚀 Day 2 of #100DaysofCode Explored Data Types, Type casting, Subscripting, and Operators. Completed "Tip Calculator" project. 
    
    Code prompts total bill, tip percentage, and people to split. Calculates individual contributions. Stay tuned for coding journey updates! #python #coding

---
+++ tip.py
@@ -0,0 +1,8 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
+bill = bill + bill * (tip / 100)
+final_amount = (bill / people)
+final_amount = "{:.2f}".format(final_amount)
+print(f"Each person should pay: ${final_amount}")
